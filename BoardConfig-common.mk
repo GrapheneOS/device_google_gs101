e5cd346381b7599fbe02150e1c27b97bc9683f00
flawedworld <flawedworld@flawed.world>
2021-11-23 22:18:49 +0000

    Disable chained vbmeta

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -84,21 +84,13 @@ TARGET_RECOVERY_UI_LIB := \
 AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
-	system \
-	system_ext \
-	product \
-	vbmeta_system
-
-ifneq ($(PRODUCT_BUILD_BOOT_IMAGE),false)
-AB_OTA_PARTITIONS += boot
-endif
-ifneq ($(PRODUCT_BUILD_VENDOR_BOOT_IMAGE),false)
-AB_OTA_PARTITIONS += vendor_boot
-AB_OTA_PARTITIONS += dtbo
-endif
-ifneq ($(PRODUCT_BUILD_VBMETA_IMAGE),false)
-AB_OTA_PARTITIONS += vbmeta
-endif
+    dtbo \
+    boot \
+    vendor_boot \
+    system \
+    system_ext \
+    product \
+    vbmeta
 
 # EMULATOR common modules
 BOARD_EMULATOR_COMMON_MODULES := liblight
@@ -143,18 +135,6 @@ SOONG_CONFIG_arm_gralloc_gralloc_ion_sync_on_lock := $(BOARD_USES_GRALLOC_ION_SY
 # Graphics
 #BOARD_USES_EXYNOS_DATASPACE_FEATURE := true
 
-# Enable chain partition for system.
-BOARD_AVB_VBMETA_SYSTEM := system system_ext product
-BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
-
-# Enable chained vbmeta for boot images
-BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 2
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
