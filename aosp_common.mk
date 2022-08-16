af3d25c4d7c4d4b6cea747d387ca85b8b3206acb
flawedworld <flawedworld@flawed.world>
2022-08-16 11:53:36 +0100

    disable mainline checks

--- aosp_common.mk
+++ aosp_common.mk
@@ -27,9 +27,6 @@ endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
 
-# Enable CSI checking
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
-
 #
 # All components inherited here go to system_ext image
 #
